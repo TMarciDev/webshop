d9487470fd8440a6d5bdc632ce704d3426d23ac0
Tóth Márton <ejjb7g@inf.elte.hu>
2022-02-15 00:21:37 +0100

    1_0:24:53

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
 
@@ -8,11 +10,14 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const Language = styled.div`
@@ -20,12 +25,36 @@ const Language = styled.div`
   cursor: pointer;
 `;
 
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
+const Logo = styled.h1`
+    font-weight: bold;
+`
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
+
+const MenuItem = styled.div`
+  font-size: 14;
+  cursor: pointer;
+  margin-left: 25px;
+`
 
 const Navbar = () => {
   return (
@@ -33,10 +62,21 @@ const Navbar = () => {
       <Wrapper>
         <Left>
           <Language>EN</Language>
-          <SearchContainer>input</SearchContainer>
+          <SearchContainer>
+            <Input />
+            <Search style={{color:"gray", fontSize:16}}/>
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center><Logo>LAMA</Logo></Center>
+        <Right>
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+                <Badge badgeContent={4} color="primary">
+                    <ShoppingCartOutlined />
+                </Badge>
+            </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
